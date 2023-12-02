f28991ec1771a8221a9b732866b10fc0bfe36a24
Matthias Tamegger <matamegger@gmail.com>
2023-12-02 14:17:38 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,53 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val input = readInput(day = 1)
+    val part1Sum = input.sumOf { line ->
+        line.extractCalibrationValue(
+            { first { it.isDigit() }.digitToInt() },
+            { last { it.isDigit() }.digitToInt() }
+        )
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    println("The sum of the calibration values for part 1 is: $part1Sum")
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part2Sum =
+        input.sumOf { line ->
+            line.extractCalibrationValue(
+                { getDigit() },
+                { getDigit(true) }
+            )
+        }
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("The sum of the calibration values for part 2 is: $part2Sum")
 }
+
+private fun String.extractCalibrationValue(firstDigit: String.() -> Int, secondDigit: String.() -> Int): Int {
+    return firstDigit(this) * 10 + last { it.isDigit() }.digitToInt()
+}
+
+private fun String.getDigit(fromEnd: Boolean = false): Int {
+    val indices = if (fromEnd) indices.reversed() else indices
+    for (index in indices) {
+        if (this[index].isDigit()) return this[index].digitToInt()
+
+        val digit = 1 + digitsAsWord.indexOfFirst { digitWord ->
+            startsWith(digitWord, index)
+        }
+        if (digit > 0) return digit
+    }
+    error("No number found")
+}
+
+
+private val digitsAsWord = listOf(
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+)
+
+private val digitsAsWordWithDigit = digitsAsWord.mapIndexed { index, it -> it to (index + 1).toString() }
